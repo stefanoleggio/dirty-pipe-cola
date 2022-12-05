b76b83f8b34c1fc00497e4a93cc3db1f2dc0dec2
crisfj00 <crisfj00@correo.ugr.es>
2022-12-05 13:18:38 -0500

    script with exfiltration of /home directory

--- SimpleHTTPServerWithUpload/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cp -v -f SimpleHTTPServerWithUpload.py /bin
-cp -v -f SimpleHTTPServerWithUpload.sh /bin
-cp -v -f SimpleHTTPServerWithUpload.service /lib/systemd/system
-chmod -v +x /bin/SimpleHTTPServerWithUpload.py
-chmod -v +x /bin/SimpleHTTPServerWithUpload.sh
-chmod -v 644 /lib/systemd/system/SimpleHTTPServerWithUpload.service
-systemctl enable SimpleHTTPServerWithUpload
-exit
